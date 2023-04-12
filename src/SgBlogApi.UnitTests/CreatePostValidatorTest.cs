941049828d8c8f78a2167b2a6aa0e632cb44a957
Jake Scott <jake.net@gmail.com>
2023-04-12 15:02:28 +1200

    feat-1011: Unit tests (#17)

---
+++ src/SgBlogApi.UnitTests/CreatePostValidatorTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.TestHelper;
+using SgBlogApi.Core;
+
+namespace SgBlogApi.UnitTests;
+
+public class CreatePostValidatorTest
+{
+    readonly CreatePost.Validator _validator = new();
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void TitleIsRequired(string value)
+    {
+        var result = _validator.TestValidate(new CreatePostRequest { Title = value });
+        result.ShouldHaveValidationErrorFor(x => x.Title);
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void BodyIsRequired(string value)
+    {
+        var result = _validator.TestValidate(new CreatePostRequest { Body = value });
+        result.ShouldHaveValidationErrorFor(x => x.Body);
+    }
+}
